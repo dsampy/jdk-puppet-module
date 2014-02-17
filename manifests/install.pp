4a872c9e1fc9fcd46bf16e8ce4f27d6ff0bc96ce
Derrick Ampy <dsampy@gmail.com>
2014-02-17 17:33:26 -0500

    Updated install Puppet classes to download artifacts

--- manifests/install.pp
+++ manifests/install.pp
@@ -1,3 +1,11 @@
 class jdk::install {
-
+  wget::fetch { "Download JDK":
+    source             => 'http://download.oracle.com/otn-pub/java/jdk/7u51-b13/jdk-7u51-linux-x64.tar.gz',
+    destination        => '/tmp/jdk-7u51-linux-x64.tar.gz',
+    timeout            => 0,
+    verbose            => false,
+    nocheckcertificate => true,
+    nocookies          => true,
+    cookie             => 'gpw_e24=http%3A%2F%2Fwww.oracle.com',
+  }
 }
